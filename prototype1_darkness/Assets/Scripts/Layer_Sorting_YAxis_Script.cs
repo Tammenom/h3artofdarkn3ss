f2c2691a57ca8a1d9bfc235dda52d4d3d53acae2
Tammenom <33290853+Tammenom@users.noreply.github.com>
2017-11-01 19:45:22 +0100

    FirstPrototypeAdd

---
+++ prototype1_darkness/Assets/Scripts/Layer_Sorting_YAxis_Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer_Sorting_YAxis_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            GetComponent<SpriteRenderer>().sortingOrder = -(int)(transform.position.y * 100);
+        
+    }
+}
---
+++ prototype1_darkness/Assets/Scripts/SkipperController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipperController : MonoBehaviour {
+
+    public float horSpeed;
+    public float verSpeed;
+
+    private Rigidbody2D skiRigidbody;
+
+    private bool moving;
+
+    public float timeBetweenMove;
+    private float timeBetweenMoveCounter;
+    public float timeToMove;
+    public float timeToMoveCounter;
+    private Vector3 moveDirection;
+
+
+	// Use this for initialization
+	void Start () {
+
+        skiRigidbody = GetComponent<Rigidbody2D>();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (moving)
+        {
+            timeToMoveCounter -= Time.deltaTime;
+            skiRigidbody.velocity = moveDirection;
+
+            if(timeToMoveCounter<0f)
+            {
+                moving = false;
+                timeBetweenMoveCounter = timeBetweenMove;
+            }
+
+
+
+
+        }
+        else
+        {
+            timeBetweenMoveCounter -= Time.deltaTime;
+            skiRigidbody.velocity = Vector2.zero;
+
+            if(timeBetweenMoveCounter < 0f)
+            {
+                moving = true;
+                timeToMoveCounter = timeToMove;
+
+                moveDirection = new Vector3(Random.Range(-1f, 1f) *horSpeed, Random.Range(-1f, 1f)*verSpeed, 0f);
+            }
+
+
+        }
+		
+	}
+}
