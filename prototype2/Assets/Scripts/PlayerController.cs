d8b84cd9f25a10d59b2d8196fdd54d7981dca275
kuehnleinlukas <lukas.kuehnlein@posteo.de>
2017-11-02 18:47:44 +0100

    Creating Prototype2
    2

---
+++ prototype2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float moveSpeed;
+    public float upDownSpeed;
+
+    private Animator anim;
+    private Rigidbody2D playerRigidBody;
+
+    private bool PlayerMovingHorizontal;
+    private bool PlayerMovingVertical;
+    private float LastMove;
+
+    private static bool playerExists = false;
+
+
+    // Use this for initialization
+    void Start () {
+
+        anim = GetComponent<Animator>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+
+        if (!playerExists)
+        {
+            
+            DontDestroyOnLoad(transform.gameObject);
+            playerExists = true;
+        } else
+        {
+            Destroy(gameObject);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        PlayerMovingHorizontal = false;
+        PlayerMovingVertical = false;
+
+
+        if (Input.GetAxisRaw("Horizontal")> 0.5f || Input.GetAxisRaw("Horizontal") < -0.5)
+        {
+            //transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, playerRigidBody.velocity.y);
+            PlayerMovingHorizontal = true;
+            LastMove = Input.GetAxisRaw("Horizontal");
+        }
+
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5)
+        {
+            //transform.Translate(new Vector3 (0f, Input.GetAxisRaw("Vertical") * upDownSpeed * Time.deltaTime, 0f));
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, Input.GetAxisRaw("Vertical") * upDownSpeed);
+            PlayerMovingVertical = true;
+        }
+
+        if(Input.GetAxisRaw("Horizontal") <0.5f && Input.GetAxisRaw("Horizontal") > -0.5)
+        {
+            playerRigidBody.velocity = new Vector2(0f, playerRigidBody.velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5)
+        {
+            playerRigidBody.velocity = new Vector2( playerRigidBody.velocity.x, 0f);
+        }
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerHorizontalMoving", PlayerMovingHorizontal);
+        anim.SetBool("PlayerVerticalMoving", PlayerMovingVertical);
+        anim.SetFloat("LastMoveX", LastMove);
+
+    }
+}
---
+++ prototype2/Assets/Scripts/background_moving1test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_moving1test : MonoBehaviour {
+
+    private Vector3 targetPos;
+    private Vector3 newVector;
+    public float picmax;
+    public float picmin;
+
+    public float moveSpeed;
+    public float groundMoveSpeed;
+    private float xCorrection;
+
+    // Use this for initialization
+    void Start () {
+        xCorrection = groundMoveSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+        targetPos = new Vector3(transform.position.x + xCorrection, transform.position.y, transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+        if(transform.position.x >= picmax)
+        {
+            newVector = new Vector3(-picmin, transform.position.y, transform.position.z);
+
+            transform.position = newVector;
+
+            xCorrection = groundMoveSpeed;
+
+
+        }
+    }
+}
